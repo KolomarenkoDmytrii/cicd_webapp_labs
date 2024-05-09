764ce7dcab27fa23ecded0e321a7274257c27d75
KolomarenkoDmytrii <dmytrii_kolomarenko@knu.ua>
2024-05-09 22:38:17 +0300

    Made templates more modular

--- company/company/settings.py
+++ company/company/settings.py
@@ -65,6 +65,9 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+
+                "main.context_processors.site_title",
+                "main.context_processors.site_copyright",
             ],
         },
     },
---
+++ company/main/context_processors.py
@@ -0,0 +1,9 @@
+def site_title(request):
+    return {
+        "site_title": 'Юридична компанія "Флагман"',
+    }
+
+def site_copyright(request):
+    return {
+        "site_copyright": 'Юридична компанія Lawflagman (https://lawflagman.com.ua/) © 2024',
+    }
