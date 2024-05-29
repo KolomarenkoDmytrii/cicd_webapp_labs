47b28f4f4a3dee7c55c73f2d678ffa4d5650c01f
KolomarenkoDmytrii <dmytrii_kolomarenko@knu.ua>
2024-05-29 19:14:57 +0300

    Updated settings.py to use database url

--- company/company/settings.py
+++ company/company/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,6 +123,9 @@ DATABASES = {
         'PORT': 5432,
     }
 }
+
+if os.environ.get("DATABASE_URL"):
+    DATABASES["default"].update(dj_database_url.config())
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
